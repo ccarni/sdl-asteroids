b00060d0a1f79752cc4ce0cd8519979eb698bcd4
ccarni <secondarysalt@gmail.com>
2023-12-14 16:11:06 -0700

    first commit

--- README.md
+++ README.md
@@ -1,33 +1,15 @@
-# SDL Template
-A simple template for Simple DirectMedia Layer (SDL2) 2.28.5 built for Windows.
-By default, it just creates an empty 800x800 window when run.
+# Asteroids
 
-![image](https://github.com/ccarni/sdl-template/assets/94944656/bcfd8910-dc14-4a51-a5fc-b35a0bb51d3f)
+Asteroids using c++ and SDL2
 
 
-## Running
-Clone the directory:
-```bash
-git clone https://github.com/ccarni/sdl-template
-cd sdl-template
-```
+## Todo
 
-Run the makefile in the root directory:
-
-```bash
-make
-```
-
-This will compile from main.cpp in /src to main.exe in /bin.
-
-Then run main.exe:
-```bash
-.\bin\main.exe
-```
-
-or
-
-```bash
-cd bin
-main.exe
-```
+- [ ] Add player movement
+- [ ] Add looping
+- [ ] Add asteroids
+- [ ] Add asteroid splitting
+- [ ] Add score counter
+- [ ] Add death screen
+- [ ] Add main screen
+- [ ] Add sound
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,83 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <vector>
+#include <cmath>
 
 
 const int WIDTH = 800, HEIGHT = 800;
 
+class Player {
+    public:
+        float xPos = 0, yPos = 0;
+        float width = 10, height = 10;
+        float rotAngle = 0;
+        std::vector<float> tipPoint, backLeft, backRight;
 
-int main( int argc, char *argv[] )
-{
+        Player(float _width, float _height, float _xPos, float _yPos) {
+            xPos = _xPos;
+            yPos = _yPos;
+            height = _height;
+            width = _width;
+        }
+
+        void DrawPlayer(SDL_Renderer *renderer) {
+            tipPoint = {xPos + (float)cos(rotAngle) * (width/2.0f), yPos - (float)sin(rotAngle) * (height/2.0f)};
+            backLeft = {xPos - (float)sin(rotAngle + M_PI/4.0f) * (width/2.0f), yPos - (float)cos(rotAngle + M_PI/4.0f) * (height/2.0f)};
+            backRight = {xPos + (float)sin(rotAngle - M_PI/4.0f) * (width/2.0f), yPos + (float)cos(rotAngle - M_PI/4.0f) * (height/2.0f)};
+
+            SDL_RenderDrawPointF(renderer, xPos, yPos);
+            SDL_RenderDrawLineF(renderer, tipPoint[0], tipPoint[1], backLeft[0], backLeft[1]);
+            SDL_RenderDrawLineF(renderer, backLeft[0], backLeft[1], backRight[0], backRight[1]);
+            SDL_RenderDrawLineF(renderer, backRight[0], backRight[1], tipPoint[0], tipPoint[1]);
+        }
+
+    private:
+        
+
+};
+
+
+int main( int argc, char *argv[] ) {
 
     SDL_Init( SDL_INIT_EVERYTHING );
 
     SDL_Window *window = SDL_CreateWindow( "Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
 
-    if ( window == nullptr ) 
-    {
+    if ( window == nullptr ) {
         std::cout << "Error creating window: " << SDL_GetError( );
         return EXIT_FAILURE;
     }
 
     SDL_Event windowEvent;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    while (true) 
-    {
-        if (SDL_PollEvent( &windowEvent ))
-        {
-            if ( windowEvent.type == SDL_QUIT )
-            {
+    Player *pPlayer = new Player(900, 900, 400, 400);
+
+    while (true) {
+        if (SDL_PollEvent( &windowEvent )){
+            if ( windowEvent.type == SDL_QUIT ){
                 break;
             }
         }
+
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        pPlayer->DrawPlayer(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        pPlayer->rotAngle += 0.0001f;
     }
 
+    delete pPlayer;
+
     SDL_DestroyWindow( window );
+    SDL_DestroyRenderer( renderer );
     SDL_Quit( );
 
     return EXIT_SUCCESS;
